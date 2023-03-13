6fe85bf8a4173dab237e8cbe26613174699545d2
Marcio Silva <marcio.silva@sbahq.org>
2023-03-13 14:41:46 -0300

    save pontos instrutor

--- app/Http/Controllers/PontoInstrutorController.php
+++ app/Http/Controllers/PontoInstrutorController.php
@@ -20,7 +20,7 @@ class PontoInstrutorController extends Controller
 
     public function savePontosInstrutor(Request $request){
         $data = $request->all();
-        return $data['pontos[]'];
+        return $data['ponto[]'];
     }
 
 }
